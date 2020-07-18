72eb00994b010bc0b6caceb8f1574d16bf161fbf
tianluhua <466469918@qq.com>
2020-07-18 18:35:21 +0800

    添加DiscoveryClient获取信息

--- cloud-provider-payment8001/src/main/java/com/tlh/springcloud/controller/PaymentContorller.java
+++ cloud-provider-payment8001/src/main/java/com/tlh/springcloud/controller/PaymentContorller.java
@@ -5,9 +5,14 @@ import com.tlh.springcloud.entities.Payment;
 import com.tlh.springcloud.service.IPaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -15,6 +20,9 @@ public class PaymentContorller {
 
     @Resource
     private IPaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @Value("${server.port}")
     private String servicePort;
@@ -38,6 +46,15 @@ public class PaymentContorller {
         } else {
             return new CommonResult(400, "没有对应记录 id：" + id);
         }
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object discovery(){
+        Map<String,List<ServiceInstance>> serviceMap=new HashMap();
+        return  discoveryClient.getServices().stream().map(item -> {
+            serviceMap.put(item, discoveryClient.getInstances(item));
+            return serviceMap;
+        });
     }
 
 }
