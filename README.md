3b2f9c70b9b0ba119e1a4f8a17f058e5f1af18b2
tianluhua <466469918@qq.com>
2020-07-18 12:25:33 +0800

    添加Eureka的服务端课客户端

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SpringCloud学习的栗子
 SpringCloud版本为H版本，SpringBoot版本为2.2.x版本
+## 单机版Eureka
--- cloud-eureka-server7001/src/main/java/com/tlh/springcloud/EurekaService7001.java
+++ cloud-eureka-server7001/src/main/java/com/tlh/springcloud/EurekaService7001.java
@@ -2,12 +2,12 @@ package com.tlh.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaService7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaService7001.class,args);
     }
 }
