77235fa3375e07c36142e7f340a68b9282736506
tianluhua <466469918@qq.com>
2020-07-31 00:05:17 +0800

    GateWay硬编码实现配置

---
+++ cloud-gateway-gateway9527/src/main/java/com/tlh/springcloud/comfig/GateWayConfig.java
@@ -0,0 +1,17 @@
+package com.tlh.springcloud.comfig;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GateWayConfig {
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r->r.path("/payment/get/**").uri("http://localhost:8001"))
+                .route(r->r.path("/payment/lb/**").uri("http://localhost:8002"))
+                .build();
+    }
+}
