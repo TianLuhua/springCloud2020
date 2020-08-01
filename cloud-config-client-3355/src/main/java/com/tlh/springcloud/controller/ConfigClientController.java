91cfa3630c75a724bfc35ed6dcc03fa200231d10
tianluhua <466469918@qq.com>
2020-08-01 17:00:33 +0800

    增加configClient端手动刷新

---
+++ cloud-config-client-3355/src/main/java/com/tlh/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,18 @@
+package com.tlh.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${configString.foo}")
+    private String configInfo;
+
+    @GetMapping("/getConfigInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
