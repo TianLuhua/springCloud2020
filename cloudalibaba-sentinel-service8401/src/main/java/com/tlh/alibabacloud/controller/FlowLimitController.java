157be8c9b04466c1b42b4b27bc6806a819e5536d
Tianluhua <466469918@qq.com>
2020-08-28 23:10:00 +0800

    热点参数限流

--- cloudalibaba-sentinel-service8401/src/main/java/com/tlh/alibabacloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/tlh/alibabacloud/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.tlh.alibabacloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +19,13 @@ public class FlowLimitController {
         return "this is test B";
     }
 
+    @GetMapping("/testhotKey")
+    @SentinelResource(value = "testhotKey", blockHandler = "deal_testhotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1, @RequestParam(value = "p2",required = false) String p2) {
+        return "this is test hotKey";
+    }
 
+    public String deal_testhotKey(String p1, String p2, BlockException e) {
+        return "this is deal_testhotKey";
+    }
 }
